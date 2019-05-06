6c2c12b79f6c4664c45e80a74efd4039b1779abe
hanqing.mao <hanqing.mao@alo7.com>
2019-05-06 14:03:12 +0800

    add image

--- lib/Section/components/image/index.dart
+++ lib/Section/components/image/index.dart
@@ -9,24 +9,22 @@ class _ImageDemoState extends State<ImageDemo> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text("Image"),
-      ),
-      child: SafeArea(
-        child: ListView(
-        padding: EdgeInsets.all(10),
-        children: <Widget>[
-          ImageDemoItem(BoxFit.fill),
-          ImageDemoItem(BoxFit.contain),
-          ImageDemoItem(BoxFit.cover),
-          ImageDemoItem(BoxFit.fitWidth),
-          ImageDemoItem(BoxFit.fitHeight),
-          ImageDemoItem(BoxFit.scaleDown),
-          ImageDemoItem(BoxFit.none),
-        ],
-      ),
-      )
-    );
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("Image"),
+        ),
+        child: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              ImageDemoItem(BoxFit.fill),
+              ImageDemoItem(BoxFit.contain),
+              ImageDemoItem(BoxFit.cover),
+              ImageDemoItem(BoxFit.fitWidth),
+              ImageDemoItem(BoxFit.fitHeight),
+              ImageDemoItem(BoxFit.scaleDown),
+              ImageDemoItem(BoxFit.none),
+            ],
+          ),
+        ));
   }
 }
 
@@ -35,28 +33,44 @@ class ImageDemoItem extends StatelessWidget {
   final BoxFit fit;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 321,
-      color: CupertinoColors.white,
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: Image.network(
-              "http://images.cnitblog.com/blog/582939/201411/121511245385501.jpg",
-              fit: fit,
-            ),
-            height: 300,
-            width: 200,
+    return Flex(
+      direction: Axis.horizontal,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(left: 16, right: 0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 81,
+                color: CupertinoColors.white,
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                      color: CupertinoColors.activeBlue,
+                      child: Image.network(
+                          "http://images.cnitblog.com/blog/582939/201411/121511245385501.jpg",
+                          fit: fit),
+                      height: 80,
+                      width: 160,
+                    ),
+                    Container(
+                      color: CupertinoColors.destructiveRed,
+                      child: Text(fit.toString(), textAlign: TextAlign.right),
+                      padding: EdgeInsets.only(left: 10),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: CupertinoColors.lightBackgroundGray,
+                height: 1,
+              )
+            ],
           ),
-          Container(
-            child: Text(fit.toString()),
-          ),
-          Container(
-            color: CupertinoColors.lightBackgroundGray,
-            height: 1,
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
