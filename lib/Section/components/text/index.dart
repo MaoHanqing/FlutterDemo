662dae08f244d57f4bb27a6dc87942ad42b41a7a
hanqing.mao <hanqing.mao@alo7.com>
2019-05-09 17:13:53 +0800

    add font

--- lib/Section/components/text/index.dart
+++ lib/Section/components/text/index.dart
@@ -9,21 +9,41 @@ class _TextDemoState extends State<TextDemo> {
   //  Paint pg = Paint();
   //创建text样式，边框，对齐方式，等
 
-  List<Text> items = [Text("简单的测试"),
-  Text("测试多行",textAlign: TextAlign.right,style: TextStyle(
-    background: Paint()
-  ),),];
+  List<Text> items = [
+    Text("简单的测试"),
+    Text(
+      "测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行测试多行",
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    ),
+    Text("Text Style 测试",
+    style: TextStyle(
+      color: CupertinoColors.activeGreen,
+      fontSize: 25,
+      fontFamily: "ZCOOLKuaiLe",
+      // letterSpacing: 5,
+      // wordSpacing: 20,
+      // fontWeight: FontWeight.bold,
+    ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(),
-      // backgroundColor: CupertinoColors.activeBlue,
       child: SafeArea(
-        child: ListView.builder(
+        child: ListView.separated(
           itemCount: items.length,
           itemBuilder: (context, i) {
             return TextItem(items[i]);
+          },
+          separatorBuilder: (context,i){
+            return Container(
+              color: CupertinoColors.activeBlue,
+              height: 1,
+              width: double.infinity,
+            );
           },
         ),
       ),
@@ -37,10 +57,9 @@ class TextItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 60,
-      color: CupertinoColors.destructiveRed,
-      child: this.text
-    );
+        width: 200,
+        height: 100,
+        child: this.text,
+      );
   }
 }
